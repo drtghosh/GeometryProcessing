d103efe8183091d5aa67fa7455b3d9c3abe79698
Isabelle Jasmin Mieves <isabelle.mieves@alumni.fh-aachen.de>
2023-05-07 15:15:52 +0200

    solution assignment 2

--- src/assignment02/task.cc
+++ src/assignment02/task.cc
@@ -26,6 +26,65 @@ tg::dir3 task::compute_normal(std::vector<pm::vertex_handle> const& vs, pm::vert
      */
     // ----- %< -------------------------------------------------------
 
+    std::vector<tg::pos3> vs_coo;
+    auto pos_vec = position.to_vector();
+
+    //1. Calculate centre of origin
+    const int size = vs.size();
+
+    tg::pos3 coo = { 0.0,0.0,0.0 };
+    for (int i = 0;i < size ;i++) {
+        auto pos = pos_vec[vs[i].idx.value];
+        vs_coo.push_back(pos);
+        coo += pos;
+    }
+    coo /= size;
+
+    //2. Move centre of gravity to origin
+    for (int i = 0;i < size ;i++) {
+        vs_coo[i]+=(-coo); //TODO does vs_coo change?
+    }
+    //3. Calculate inertia tensor
+    auto xx = 0.0;
+    auto yy = 0.0;
+    auto zz = 0.0;
+    auto xy = 0.0;
+    auto xz = 0.0;
+    auto yz = 0.0;
+
+    for (int i = 0; i < size;i++) {
+        xx += pow(vs_coo[i].x,2);
+        yy += pow(vs_coo[i].y, 2);
+        zz += pow(vs_coo[i].z, 2);
+
+        xy += (vs_coo[i].x * vs_coo[i].y);
+        xz += (vs_coo[i].x * vs_coo[i].z);
+        yz += (vs_coo[i].y * vs_coo[i].z);
+    }
+    tg::mat3 inertia_tensor;
+    inertia_tensor[0][0] = xx;
+    inertia_tensor[1][1] = yy;
+    inertia_tensor[2][2] = zz;
+
+    inertia_tensor[0][1] = xy, inertia_tensor[1][0] = xy;
+    inertia_tensor[0][2] = xz, inertia_tensor[2][0] = xz;
+    inertia_tensor[1][2] = yz, inertia_tensor[2][1] = yz;
+
+    //4. Find eigenvector for min eigenvalue as normal
+    auto eigen_decomp = tg::eigen_decomposition_symmetric(inertia_tensor);
+
+    float min_eigenvalue = FLT_MAX;
+    tg::dir3 min_eigenvector;
+    for (auto value : eigen_decomp._values) {
+        auto eigenvalue = value.eigenvalue;
+        auto eigenvector = value.eigenvector;
+        if (eigenvalue < min_eigenvalue) {
+            min_eigenvalue = eigenvalue;
+            min_eigenvector = { eigenvector.x, eigenvector.y, eigenvector.z};
+        }
+    }
+
+    normal = {min_eigenvector.x, min_eigenvector.y, min_eigenvector.z};
 
     // ----- %< -------------------------------------------------------
     /*
@@ -54,6 +113,21 @@ float task::compute_mst_weight(pm::vertex_handle v0, pm::vertex_handle v1, pm::v
      */
     // ----- %< -------------------------------------------------------
 
+    auto normal_vec = normal.to_vector();
+    auto position_vec = position.to_vector();
+
+    auto n0 = normal_vec[v0.idx.value];
+    auto n1 = normal_vec[v1.idx.value];
+
+    auto pos0 = position_vec[v0.idx.value];
+    auto pos1 = position_vec[v1.idx.value];
+
+    float alpha = 0.0; //can be adjusted to consider vertex distance
+
+    auto pos = pos1 - pos0;
+    float norm = sqrt(pow(pos.x,2) + pow(pos.y, 2) + pow(pos.z, 2));
+ 
+    weight = (1 - abs(tg::dot(n0,n1)))+alpha*norm;
 
     // ----- %< -------------------------------------------------------
     /*
