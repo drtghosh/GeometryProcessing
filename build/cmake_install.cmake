c34ad0f134bbb74784a44b57bd846fde7816427a
Debabrata Ghosh <rijugh@gmail.com>
2023-05-09 23:51:26 +0200

    Assignment 2 structure added

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -44,6 +44,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/drtghosh/Documents/All_of_RWTH/Semester2/GeometryProcessingCourse/Exercises/GeometryProcessingFramework/build/extern/eigen-lean/cmake_install.cmake")
   include("C:/Users/drtghosh/Documents/All_of_RWTH/Semester2/GeometryProcessingCourse/Exercises/GeometryProcessingFramework/build/src/assignment00/cmake_install.cmake")
   include("C:/Users/drtghosh/Documents/All_of_RWTH/Semester2/GeometryProcessingCourse/Exercises/GeometryProcessingFramework/build/src/assignment01/cmake_install.cmake")
+  include("C:/Users/drtghosh/Documents/All_of_RWTH/Semester2/GeometryProcessingCourse/Exercises/GeometryProcessingFramework/build/src/assignment02/cmake_install.cmake")
 
 endif()
 
